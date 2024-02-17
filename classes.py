1695e6d4cb8311a368b7a70f6f540496d7b9959a
Andrey Fedoseev <MaxviP100@yandex.ru>
2024-02-17 23:05:33 +0700

    create class Product

--- classes.py
+++ classes.py
@@ -1,6 +1,23 @@
 class Category:
     """Категории под товары"""
+
+    count_category = 0
     def __init__(self, name, description, goods):
         self.name = name
         self.description = description
         self.goods = goods
+
+        Category.count_category += 1
+
+
+class Product:
+    """Товар в магазине"""
+
+    count_unic_goods = 0
+    def __init__(self, name, description, pay, count):
+        self.name = name
+        self.description = description
+        self.pay = pay
+        self.count = count
+
+        Product.count_unic_goods += 1
