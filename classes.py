f9b8d5ec0410e0d1190245a9cfe66217e7a68165
Andrey Fedoseev <MaxviP100@yandex.ru>
2024-02-18 15:02:45 +0700

    add count category and goods

--- classes.py
+++ classes.py
@@ -1,23 +1,31 @@
 class Category:
     """Категории под товары"""
 
+    name: str
+    description: str
+    goods: list
+
     count_category = 0
+    count_goods = 0
+
     def __init__(self, name, description, goods):
         self.name = name
         self.description = description
         self.goods = goods
 
         Category.count_category += 1
+        Category.count_unic_goods = len(set(self.goods))
 
 
 class Product:
     """Товар в магазине"""
+    name: str
+    description: str
+    pay: float
+    count: int
 
-    count_unic_goods = 0
     def __init__(self, name, description, pay, count):
         self.name = name
         self.description = description
         self.pay = pay
         self.count = count
-
-        Product.count_unic_goods += 1
